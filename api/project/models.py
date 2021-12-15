0e859c28a30fdd79ccf72c9629554022fc212795
Matthew Lau <lau.cy.matthew@gmail.com>
2021-12-15 23:03:04 +0800

    fix: use static method to get project

--- api/project/models.py
+++ api/project/models.py
@@ -8,6 +8,7 @@ class Project:
     def __init__(self, name):
         self.name = name
 
+    @staticmethod
     @auth_required
     def get_project_by_id(_, project_id: uuid.UUID):
         return db["projects"].find_one({"_id": ObjectId(project_id)})
