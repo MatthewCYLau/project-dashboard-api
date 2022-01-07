87e9b40c45b07cc4ef23970ebd23cfa157049b1e
Matthew Lau <lau.cy.matthew@gmail.com>
2022-01-07 17:28:53 +0000

    test: added test on get users

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -1,8 +1,18 @@
 from api import app
+import unittest
+from api.db.setup import db
+from unittest.mock import patch
+from mongomock import MongoClient
 
 
-def test_health_check():
-    with app.test_client() as test_client:
-        response = test_client.get("/ping")
-        assert response.status_code == 200
-        assert b"pong!" in response.data
+class PyMongoMock(MongoClient):
+    def init_app(self, app):
+        return super().__init__()
+
+
+class TestUsers(unittest.TestCase):
+    def test_get_users(self):
+        with patch.object(db, "mongo", PyMongoMock()):
+            mock_app = app.test_client()
+            response = mock_app.get("/api/users")
+            self.assertEqual(response.status_code, 200)
